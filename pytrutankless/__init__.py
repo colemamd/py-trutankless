79abbf6f786bc7ba01e60ceaf9b605791af5860c
Matt Coleman <mdcoleman001@gmail.com>
2022-09-25 20:06:25 -0400

    refactor auth

--- pytrutankless/__init__.py
+++ pytrutankless/__init__.py
@@ -1,3 +1,1 @@
-"""Python wrapper for TruTankless water heater API"""
-from .api import TruTanklessApiInterface
-from .device import Device
+"""Python wrapper for TruTankless water heater API."""
--- pytrutankless/api.py
+++ pytrutankless/api.py
@@ -1,129 +1,122 @@
-import asyncio
+from __future__ import annotations
+from datetime import datetime, timezone
 import logging
-from typing import Dict, List, Type, TypeVar
+from typing import Dict, Optional
 
-import aiohttp
 from aiohttp import ClientSession
-from aiohttp.client_exceptions import ClientError
 
-from pytrutankless.device import Device
-from pytrutankless.errors import (GenericHTTPError, InvalidCredentialsError,
-                                  TruTanklessError)
-
-BASE_URL = f"https://home.trutankless.com/"
-DEVICES_URL = f"{BASE_URL}api/dashboard/devices/"
-LOCATIONS_URL = f"{BASE_URL}api/dashboard/locations"
-TOKEN_URL = f"{BASE_URL}api/dash-oauth/token"
-HEADERS = {"Content-Type": "application/json"}
-GRANT_TYPE = "password"
-CLIENT_ID = "123"
+from .device import Device
+from .token import Token
 
 _LOGGER = logging.getLogger(__name__)
 
-ApiType = TypeVar("ApiType", bound="TruTanklessApiInterface")
+BASE_URL = "https://home.trutankless.com/"
+DEVICES_URL = f"{BASE_URL}api/dashboard/devices/"
+LOCATIONS_URL = f"{BASE_URL}api/dashboard/locations"
+TOKEN_URL = f"{BASE_URL}api/dash-oauth/token"
+CLIENT_ID = "123"
+GRANT_TYPE = "password"
+HEADERS = {"Content-Type": "application/json"}
+
+
+async def get_token(
+    session: ClientSession,
+    username: str,
+    password: str,
+    client_id: str = CLIENT_ID,
+    grant: str = GRANT_TYPE,
+) -> Token:
+    """Retrieve token from the API."""
+    payload = {
+        "username": username,
+        "password": password,
+        "grant_type": grant,
+        "client_id": client_id,
+    }
+
+    async with session.post(TOKEN_URL, data=payload) as _token_resp:
+        new_token = await Token.from_dict(await _token_resp.json())
+    HEADERS["Authorization"] = f"Bearer {new_token.access_token}"
+    return new_token
+
+
+async def refresh_token(session: ClientSession, token: Token) -> Token:
+    """Obtain new access token using the refresh token."""
+    ref_tok = token.ref_tok
+    payload = {
+        "client_id": CLIENT_ID,
+        "grant_type": "refresh_token",
+        "refresh_token": ref_tok,
+    }
+
+    async with session.post(TOKEN_URL, data=payload) as _refresh_resp:
+        _response = await _refresh_resp.json()
+        token.access_token = _response["access_token"]
+        HEADERS["Authorization"] = f"Bearer {token.access_token}"
+        return token
 
 
 class TruTanklessApiInterface:
-    """API Interface object."""
+    """Interface to TruTankless API."""
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        user: str,
+        passwd: str,
+        token: Optional[Token] = None,
+    ) -> None:
         """Create the TruTankless API interface object."""
-        self._headers: str = HEADERS
+        self._headers: dict = HEADERS
         self._location_id: str
         self._user_id: str
-        self._locations: List = {}
+        self._locations: Dict = {}
+        self._user = user
+        self._passwd = passwd
         self.devices: Dict = {}
+        self.session = ClientSession()
+        self.token = token
 
-    @classmethod
-    async def login(cls: Type[ApiType], email: str, password: str) -> ApiType:
-        """Create a TruTanklessApiInterface object using email and password."""
-        this_class = cls()
-        await this_class._authenticate(
-            {
-                "username": email,
-                "password": password,
-                "grant_type": GRANT_TYPE,
-                "client_id": CLIENT_ID,
-            }
+    async def authenticate(self) -> Token:
+        """Return valid access token."""
+        if self.token and self.token.expires_at > datetime.now(timezone.utc):
+            return self.token
+        if self.token and self.token.expires_at < datetime.now(timezone.utc):
+            _LOGGER.debug("access token is expired. Refreshing access token.")
+            return await refresh_token(self.session, self.token)
+        self.token = await get_token(
+            self.session, username=self._user, password=self._passwd
         )
-        return this_class
+        HEADERS["Authorization"] = f"Bearer {self.token.access_token}"
+        return self.token
 
-    async def _authenticate(self, payload: dict) -> None:
-        async with aiohttp.ClientSession() as _session:
-            try:
-                async with _session.post(TOKEN_URL, data=payload) as token_resp:
-                    _token_json = await token_resp.json()
-                    if token_resp.status == 200:
-                        _LOGGER.debug(_token_json)
-                        _access_token = _token_json["access_token"]
-                        _token_type = _token_json["token_type"].capitalize()
-                        self._user_id = _token_json["user_id"]
-                        self._headers[
-                            "authorization"
-                        ] = f"{_token_type} {_access_token}"
-                        await _session.close()
-                    elif token_resp.status == 400:
-                        for error_title in _token_json.values():
-                            if error_title == "invalid_grant":
-                                raise InvalidCredentialsError(
-                                    _token_json["error_description"]
-                                )
-                            else:
-                                raise GenericHTTPError(_token_json["error_description"])
-            except ClientError as err:
-                raise err
-            finally:
-                await _session.close()
-
-    async def get_devices(self) -> Dict:
+    async def get_devices(self):
         """Get a list of all the devices for this user and instantiate device objects."""
         await self._get_locations()
-        for _devlist in self._locations["devices"]:
+        for _devlist in self._locations[0]["devices"]:
             _dev_obj = Device(_devlist, self)
             self.devices[_dev_obj.device_id] = _dev_obj
             return self.devices
 
-    async def refresh_device(self, device: str) -> Dict:
+    async def refresh_device(self, device: str):
         """Fetch updated data for a device."""
+        _device_url = f"{DEVICES_URL}{device}"
+        async with self.session.get(
+            _device_url, headers=self._headers
+        ) as refr:
+            _refdata = await refr.json()
+            _LOGGER.debug("Retrieved updated data from API: %s", _refdata)
+            dev_obj = self.devices.get(_refdata.get("id", ""), None)
+            if dev_obj:
+                await dev_obj.update_device_info(_refdata)
+        return dev_obj
 
-        async with ClientSession() as _refresh_session:
-            _device_url = f"{DEVICES_URL}{device}"
-            try:
-                async with _refresh_session.get(
-                    _device_url, headers=self._headers
-                ) as refr:
-                    if refr.status == 200:
-                        _refdata = await refr.json()
-                        _LOGGER.debug(f"Retrieved updated data from API: {_refdata}")
-                        dev_obj = self.devices.get(_refdata.get("id", ""), None)
-                        if dev_obj:
-                            await dev_obj.update_device_info(_refdata)
-            except ClientError as err:
-                _LOGGER.error("Failed to fetch device.")
-                raise err
-            finally:
-                await _refresh_session.close()
-                return dev_obj
-
-    async def _get_locations(self) -> Dict:
-        async with ClientSession() as _location_session:
-            try:
-                async with _location_session.get(
-                    LOCATIONS_URL, headers=self._headers
-                ) as resp:
-                    if resp.status == 200:
-                        _json = await resp.json()
-                        _LOGGER.debug(_json)
-                        for _ind in _json:
-                            self._locations = _ind
-                    elif resp.status == 401:
-                        raise InvalidCredentialsError(resp.status)
-                    else:
-                        raise GenericHTTPError(resp.status)
-            except ClientError as err:
-                raise err
-            finally:
-                await _location_session.close()
+    async def _get_locations(self) -> None:
+        async with self.session.get(
+            LOCATIONS_URL, headers=self._headers
+        ) as resp:
+            if resp.status == 200:
+                self._locations = await resp.json()
+                _LOGGER.debug(self._locations)
 
     @property
     def user_id(self) -> str:
