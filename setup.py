a6045d2e815696e75c96ef2a916cdafd7630a05b
Matt Coleman <mdcoleman001@gmail.com>
2022-09-26 20:54:33 -0400

    update setup

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
+import re
 from setuptools import setup, find_packages
 import pathlib
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("requirements.txt", encoding="utf-8") as f:
+    install_requires = f.read().splitlines()
+
+
+with open("pytrutankless/__init__.py", encoding="utf-8") as f:
+    version = re.findall(r"__version__ = \"(.+)\"", f.read())[0]
 
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="pytrutankless",
-    version="0.2.0",
+    version=version,
     description="A Python client library for the TruTankless API",
-    long_description=long_description,
+    long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/colemamd/pytrutankless",
+    download_url="https://github.com/CyanBook/pytrutankless/releases/latest",
     author="@colemamd",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -22,8 +34,10 @@ setup(
         "Programming Language :: Python :: 3.10",
     ],
     license="MIT",
-    install_requires="aiohttp",
-    packages=find_packages(exclude=["dist", "*.test", "*.test.*", "test.*", "test"]),
+    install_requires=install_requires,
+    packages=find_packages(
+        exclude=["dist", "*.test", "*.test.*", "test.*", "test"]
+    ),
     zip_safe=True,
     project_urls={
         "Source": "https://github.com/colemamd/pytrutankless",
